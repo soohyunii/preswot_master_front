80a30987b070ba4e93a4a7ab16f956aafc8d540c
Olaf Ahn <adoji92@gmail.com>
2018-01-03 13:02:25 +0900

    Add TODO:

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,6 +2,7 @@ export default {
   login({ email, password }) {
     console.log('service email, password', email, password); // eslint-disable-line
 
+    // TODO: Replace dummy with axios
     return new Promise((resolve) => {
       window.setTimeout(() => {
         resolve({
