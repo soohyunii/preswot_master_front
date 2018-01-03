c95a1c3b19530791be06e34722eca117fa8879bd
Olaf Ahn <adoji92@gmail.com>
2018-01-03 18:07:03 +0900

    Install vue-i18n, browser-locale npm packages

--- src/main.js
+++ src/main.js
@@ -3,14 +3,27 @@
 import Vue from 'vue';
 import Element from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en';
+import VueI18n from 'vue-i18n';
+import getLocale from 'browser-locale';
 
 import App from './App';
 import router from './router';
 import store from './stores';
+import messages from './messages';
+
 
 Vue.config.productionTip = false;
 Vue.use(Element, {
   locale,
+});
+Vue.use(VueI18n);
+
+const browserLocale = getLocale().split('-')[0];
+
+const i18n = new VueI18n({
+  locale: localStorage.getItem('locale') || browserLocale || 'ko', // TODO: choose from browser
+  fallbackLocale: 'en',
+  messages,
 });
 
 /* eslint-disable no-new */
@@ -18,6 +31,7 @@ new Vue({
   el: '#app',
   store,
   router,
+  i18n,
   template: '<App/>',
   components: { App },
   render: h => h(App),
