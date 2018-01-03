feeed72393808785f36e7c4c880e5d0ecbb8e431
Olaf Ahn <adoji92@gmail.com>
2018-01-03 19:08:13 +0900

    Add skeleton to Register

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,21 @@
         <el-col>
           <!-- TODO: add form html element -->
           <!-- TODO: add validation -->
-        <h1>{{ $t('LOGIN.LOGIN_TITLE') }}</h1>
-        <el-input placeholder="abc@gmail.com" v-model="inputEmail" type="email">
-          <template slot="prepend">{{ $t('LOGIN.EMAIL') }}</template>
-        </el-input>
-        <el-input placeholder="********" v-model="inputPassword" type="password">
-          <template slot="prepend">{{ $t('LOGIN.PASSWORD') }}</template>
-        </el-input>
-        <!-- TODO: add disabled attr according to validation -->
-        <el-button type="primary" @click="onClick('LOGIN')">{{ $t('LOGIN.LOGIN_TITLE') }}</el-button>
+          <h1>{{ $t('LOGIN.LOGIN_TITLE') }}</h1>
+
+          <el-input placeholder="abc@gmail.com" v-model="input.email" type="email">
+            <template slot="prepend">{{ $t('LOGIN.EMAIL') }}</template>
+          </el-input>
+          <el-input placeholder="********" v-model="input.password" type="password">
+            <template slot="prepend">{{ $t('LOGIN.PASSWORD') }}</template>
+          </el-input>
+          <!-- TODO: add disabled attr according to validation -->
+          <el-button type="primary" @click="onClick('LOGIN')">{{ $t('LOGIN.LOGIN_TITLE') }}</el-button>
         </el-col>
       </el-row>
     </el-container>
-    jwt: {{ jwt }}
+    jwt: {{ jwt }} <br />
+    input: {{ input | json }}
   </div>
 </template>
 
@@ -29,8 +31,10 @@ export default {
   name: 'Login',
   data() {
     return {
-      inputEmail: 'adoji92@gmail.com',
-      inputPassword: 'adojiadoji',
+      input: {
+        email: 'adoji92@gmail.com',
+        password: 'adojiadoji',
+      },
     };
   },
   methods: {
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,6 +1,47 @@
 <template>
   <div>
-    <h2>Hello, Register Template.</h2>
+    <h2>회원가입</h2>
+    <el-container>
+      <el-row>
+        <el-col>
+          <form novalidate>
+            <el-input placeholder="abc@gmail.com" v-model="input.email" type="email">
+              <template slot="prepend">{{ $t('LOGIN.EMAIL') }}</template>
+            </el-input>
+            <el-input placeholder="********" v-model="input.password" type="password">
+              <template slot="prepend">{{ $t('LOGIN.PASSWORD') }}</template>
+            </el-input>
+            <el-input placeholder="********" v-model="input.password2" type="password">
+              <template slot="prepend">{{ $t('LOGIN.PASSWORD') }} 2</template>
+            </el-input>
+
+            <br />
+            <br />
+
+            <el-input placeholder="홍길동" v-model="input.name" type="string">
+              <template slot="prepend">Name</template>
+            </el-input>
+
+            <!-- TODO: 생년월일 -->
+            <!-- http://element.eleme.io/#/en-US/component/date-picker -->
+
+            <!-- TODO: 주소 -->
+            <!-- http://postcode.map.daum.net/guide -->
+
+            <!-- TODO: 핸드폰 번호 -->
+            <!-- 핸드폰 번호 인증 어떻게?? -->
+
+            <!-- TODO: 전공 -->
+            <!-- Just simple input -->
+
+            <!-- TODO: 소속 -->
+            <!-- Just simple input -->
+
+            <el-button type="primary" @click="onClick('LOGIN')">회원가입</el-button>
+          </form>
+        </el-col>
+      </el-row>
+    </el-container>
   </div>
 </template>
 
@@ -10,7 +51,14 @@
 export default {
   name: 'Register',
   data() {
-    return {};
+    return {
+      input: {
+        email: 'adoji92@gmail.com',
+        password: 'adojiadoji',
+        password2: 'adojiadoji',
+        name: '안동진',
+      },
+    };
   },
 };
 </script>
