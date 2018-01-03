7db002ab7276a6407f3b5a76d04f46c8ec91efeb
Olaf Ahn <adoji92@gmail.com>
2018-01-03 11:18:56 +0900

    Install axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import axios from 'axios';
 import Element from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en';
 
@@ -8,11 +9,11 @@ import App from './App';
 import router from './router';
 import store from './stores';
 
+Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 Vue.use(Element, {
   locale,
 });
-
 
 /* eslint-disable no-new */
 new Vue({
