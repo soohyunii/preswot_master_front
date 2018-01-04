4a8ef2d9157be84ef9b8d264ad168eaaf6e05186
Olaf Ahn <adoji92@gmail.com>
2018-01-04 10:21:02 +0900

    Add input | json

--- src/messages/index.js
+++ src/messages/index.js
@@ -4,14 +4,26 @@ export default {
     "LOGIN": {
       "LOGIN_TITLE": "Login",
       "EMAIL": "E-mail",
-      "PASSWORD": "Password"
+      "PASSWORD": "Password",
+      "BIRTH": "Birth",
+      "ADDRESS": "Address",
+      "ADDR_SEARCH": "Address Search",
+      "PHONE": "Phone",
+      "MAJOR": "Major",
+      "BELONG": "Belong"
     }
   },
   "ko": {
     "LOGIN": {
       "LOGIN_TITLE": "로그인",
       "EMAIL": "이메일",
-      "PASSWORD": "비밀번호"
+      "PASSWORD": "비밀번호",
+      "BIRTH": "생년월일",
+      "ADDRESS": "주소",
+      "ADDR_SEARCH": "주소검색",
+      "PHONE": "전화번호",
+      "MAJOR": "전공",
+      "BELONG": "소속"
     }
   }
 }
