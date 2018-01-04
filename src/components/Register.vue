a5cf11ebd334ad4754be0fd97d881e290638f992
Unknown <lmh315@naver.com>
2018-01-04 17:53:11 +0900

    add birth, address, phone, major, belong in Register.vue

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -19,28 +19,70 @@
             <br />
 
             <el-input placeholder="홍길동" v-model="input.name" type="string">
-              <template slot="prepend">Name</template>
+              <template slot="prepend">{{ $t('LOGIN.NAME') }}</template>
             </el-input>
 
             <!-- TODO: 생년월일 -->
             <!-- http://element.eleme.io/#/en-US/component/date-picker -->
+            <el-input suffix-icon="el-icon-date" v-model="input.birth" type="date" format="yyyy-mm-dd">
+              <template slot="prepend">{{ $t('LOGIN.BIRTH') }}</template>
+            </el-input>
+
+            <el-date-picker
+              v-model="input.birth"
+              type="date"
+              placeholder="Pick a day">
+            </el-date-picker>
+
+            <br />
+            <br />
 
             <!-- TODO: 주소 -->
             <!-- http://postcode.map.daum.net/guide -->
-
+            <el-button type="primary" onclick="sample6_execDaumPostcode()">
+              우편번호 찾기<br>
+            </el-button>
+            <el-input type="string" id="sample6_postcode" v-model="input.address1" placeholder="우편번호">
+            </el-input>
+            <el-input type="string" id="sample6_address" v-model="input.address2" placeholder="주소">
+            </el-input>
+            <el-input type="string" id="sample6_address2" v-model="input.address3" v-focus="focused" @focus="focused = true" @blur="focused = false" placeholder="상세주소">
+            </el-input>
+            <el-input placeholder="주소" :disabled="true" v-model="address">
+              <template slot="prepend">{{ $t('LOGIN.ADDRESS') }}</template>
+            </el-input>
+            <br />
+            <br />
+            
             <!-- TODO: 핸드폰 번호 -->
             <!-- 핸드폰 번호 인증 어떻게?? -->
-            <!-- 010-1234-1234 로 포미탱도 있어야 할듯 -->
-
+            <!-- 010-1234-1234 로 포매팅도 있어야 할듯 -->
+            <el-input type="tel" v-mask="['###-####-####','###-###-####']" v-model="input.phone" placeholder="핸드폰 번호">
+              <template slot="prepend">{{ $t('LOGIN.PHONE') }}</template>
+            </el-input>
+           
+            <br />
+            <br />
+           
             <!-- TODO: 전공 -->
             <!-- Just simple input -->
+            <el-input placeholder="CS" v-model="input.major" type="string">
+              <template slot="prepend">{{ $t('LOGIN.MAJOR') }}</template><br />
+            </el-input>
+
+            <br />
+            <br />
 
             <!-- TODO: 소속 -->
             <!-- Just simple input -->
+            <el-input placeholder="소속" v-model="input.belong" type="string">
+              <template slot="prepend">{{ $t('LOGIN.BELONG') }}</template><br />
+            </el-input>
 
             <el-button type="primary" @click="onClick('LOGIN')">회원가입</el-button>
             <br />
-            <pre>{{ input | json }}</pre>
+            <pre>{{ input }}</pre>
+            <pre>{{ address }}</pre>
           </form>
         </el-col>
       </el-row>
@@ -49,19 +91,42 @@
 </template>
 
 <script>
+import { focus } from 'vue-focus';
 // id(string), password(string), name(string),
 // birth(1991-07-16 형식string), address(string), phone(string), major(string), belong(string소속)
 export default {
+  directives: { focus },
   name: 'Register',
   data() {
     return {
+      focused: false,
       input: {
         email: 'adoji92@gmail.com',
         password: 'adojiadoji',
         password2: 'adojiadoji',
         name: '안동진',
+        birth: '',
+        address1: '',
+        address2: '',
+        address3: '',
+        phone: '010-1234-5678',
+        major: 'CS',
+        belong: 'aa',
       },
     };
+  },
+  watch: {
+    focused: {
+      handler() {
+        this.input.address1 = document.getElementById('sample6_postcode').value;
+        this.input.address2 = document.getElementById('sample6_address').value;
+      },
+    },
+  },
+  computed: {
+    address() {
+      return `${this.input.address2} ${this.input.address3} ${this.input.address1}`;
+    },
   },
 };
 </script>
--- src/messages/index.js
+++ src/messages/index.js
@@ -4,14 +4,26 @@ export default {
     "LOGIN": {
       "LOGIN_TITLE": "Login",
       "EMAIL": "E-mail",
-      "PASSWORD": "Password"
+      "PASSWORD": "Password",
+      "NAME": "Name",
+      "BIRTH": "Birth",
+      "ADDRESS": "Address",
+      "PHONE": "Phone",
+      "MAJOR": "Major",
+      "BELONG": "Belong"
     }
   },
   "ko": {
     "LOGIN": {
       "LOGIN_TITLE": "로그인",
       "EMAIL": "이메일",
-      "PASSWORD": "비밀번호"
+      "PASSWORD": "비밀번호",
+      "NAME": "이름",
+      "BIRTH": "생년월일",
+      "ADDRESS": "주소",
+      "PHONE": "핸드폰",
+      "MAJOR": "전공",
+      "BELONG": "소속"
     }
   }
 }
