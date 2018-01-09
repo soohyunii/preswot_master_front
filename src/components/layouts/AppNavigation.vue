de3dc3a6f221f66b2fed72435754044e97da3b41
Olaf Ahn <adoji92@gmail.com>
2018-01-09 13:50:41 +0900

    Add FIXME:

--- src/components/layouts/AppNavigation.vue
+++ src/components/layouts/AppNavigation.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- FIXME: collapse가 너무 느림 -->
     <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
       <el-radio-button :label="false">expand</el-radio-button>
       <el-radio-button :label="true">collapse</el-radio-button>
