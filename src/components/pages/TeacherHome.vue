1e95511365a2bd9b0468897413f45b5e822b53d4
이민현 <lmh315@naver.com>
2018-01-10 17:12:31 +0900

    Add TeacherHome base layout

--- src/components/pages/TeacherHome.vue
+++ src/components/pages/TeacherHome.vue
@@ -1,5 +1,80 @@
 <template>
   <div>
-    아사나에 쓰인 '강사 메인'
+    <el-container>
+      <el-aside width="100px">
+        <el-row>
+          파이썬 초급
+        </el-row>
+        <el-row>
+          자바
+        </el-row>
+        <el-row>
+          <i class="el-icon-circle-plus-outline"></i>
+          과목 추가
+        </el-row>
+      </el-aside>
+      <el-main>
+        자바<hr>
+        과목지식맵 과목큐레이션 과목저널링<br /><br />
+        <el-row :gutter="20">
+          <el-col :span="16">
+            <div class="grid-content bg-white">강의 시나리오 목록<hr></div>
+          </el-col>
+          <el-col :span="8">
+            <div class="grid-content bg-white">수강생 목록<hr></div>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col>
+            <div class="grid-content bg-white">과목 통계<hr></div>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
   </div>
 </template>
+
+<style scoped>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: rgb(255, 255, 255);
+    color: #333;
+    text-align: center;
+    line-height: 50px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+  }
+
+  .bg-white {
+    background-color: rgb(255, 255, 255);
+  }
+
+  .grid-content {
+    border-radius: 4px;
+    min-height: 300px;
+    padding: 25px;
+    margin-bottom: 30px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
