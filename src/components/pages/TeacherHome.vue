bc2ce25ac02cb2118fa0008129c61c488d69afa5
이민현 <lmh315113@gmail.com>
2018-01-11 15:09:37 +0900

    Implement TeacherHome layout by hard coding

--- src/components/pages/TeacherHome.vue
+++ src/components/pages/TeacherHome.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-container class="outline">
-      <el-aside width="100px">
+      <el-aside width="150px">
         <el-row>
           파이썬 초급
         </el-row>
@@ -9,12 +9,12 @@
           자바
         </el-row>
         <el-row>
-          <i class="el-icon-circle-plus-outline"></i>
-          과목 추가
+          <!-- TODO: Implement adding lecture part -->
+          <el-button>과목 추가</el-button>
         </el-row>
       </el-aside>
       <el-main>
-        자바<hr>
+        <h3>자바</h3><hr>
         <el-button>과목지식맵</el-button>
         <el-button>과목큐레이션</el-button>
         <el-button>과목저널링</el-button>
@@ -48,21 +48,41 @@
                   label="수강생 이해도(평균)"
                   sortable>
                   <template slot-scope="scope">
-                    <el-progress :text-inside="true" :stroke-width="18" :percentage="scope.row.understanding"></el-progress>
+                    <el-progress
+                      v-if="scope.row.understanding === 100"
+                      :text-inside="true"
+                      :stroke-width="18"
+                      :percentage="scope.row.understanding"
+                      status="success">
+                    </el-progress>
+                    <el-progress
+                      v-else-if="scope.row.understanding > 70"
+                      :text-inside="true"
+                      :stroke-width="18"
+                      :percentage="scope.row.understanding">
+                    </el-progress>
+                    <el-progress
+                      v-else
+                      :text-inside="true"
+                      :stroke-width="18"
+                      :percentage="scope.row.understanding"
+                      status="exception">
+                    </el-progress>
                   </template>
                 </el-table-column>
               </el-table>
               <router-link to="/a/teacher/lecture/new">
                 <br /><br />
-                <div style="text-align: center;" @clink="onClick('NEW')">
-                  <i class="el-icon-circle-plus-outline"></i>
-                  강의 시나리오 추가
-                </div>
+                <el-button>강의 시나리오 추가</el-button>
               </router-link>
             </div>
           </el-col>
           <el-col :span="8">
-            <div class="grid-content bg-white">수강생 목록<hr></div>
+            <div class="grid-content bg-white">
+              <el-col :span="16">수강생 목록</el-col>
+              <el-col :span="8" style="text-align: right;">8명</el-col>
+              <hr>
+            </div>
           </el-col>
         </el-row>
         <el-row :gutter="20">
@@ -83,19 +103,24 @@ export default {
     return {
       tableData: [{
         num: '1',
-        date: '2016-05-01',
+        date: '2018-01-02',
         name: '1강',
-        understanding: 95,
+        understanding: 100,
       }, {
         num: '2',
-        date: '2016-05-02',
+        date: '2018-01-09',
         name: '2강',
-        understanding: '86',
+        understanding: 86,
       }, {
         num: '3',
-        date: '2016-05-04',
+        date: '2018-01-13',
         name: '3강',
-        understanding: '57',
+        understanding: 57,
+      }, {
+        num: '4',
+        date: '2018-01-15',
+        name: '4강',
+        understanding: 72,
       }],
     };
   },
@@ -128,7 +153,7 @@ export default {
 
   .grid-content {
     border-radius: 4px;
-    min-height: 300px;
+    min-height: 340px;
     padding: 25px;
     margin-bottom: 30px;
   }
