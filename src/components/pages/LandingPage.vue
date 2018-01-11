b8b96e6603a500bcfc1fad138f57e909b829b01d
LSB <tmdqhr2586@naver.com>
2018-01-11 12:13:35 +0900

    Edit some layout of Home page

--- src/components/pages/LandingPage.vue
+++ src/components/pages/LandingPage.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
-    <el-container>
+    <el-container class="container" id="container">
       <el-main>
         <el-row type="flex" justify="center">
-          <el-col :span="20">
-            <div class="grid-content bg-white">영상 썸네일</div>
+          <el-col align="center">
+            <div class="thumbnail-content bg-film" align="center">
+              영상 썸네일
+            </div>
           </el-col>
         </el-row>
 
-        <div class="">강의 시나리오 목록<hr></div>
+        <div class="">인기 강의 목록<hr></div>
         <el-row :gutter="20">
-          <el-col :span="8">
-            <div class="grid-content bg-white">class<hr></div>
-          </el-col>
-          <el-col :span="8">
-            <div class="grid-content bg-white">class<hr></div>
-          </el-col>
-          <el-col :span="8">
-            <div class="grid-content bg-white">class<hr></div>
-          </el-col>
+          <template v-for="(item, key) in popClasses">
+            <el-col :span="4" :key="key">
+              <div class="film-content bg-film">{{item.className}}</div>
+            </el-col>
+          </template>
         </el-row>
 
         <div class="">내 수강 통계<hr></div>
@@ -28,10 +26,10 @@
           </el-col>
         </el-row>
 
-        <div class="">대시보드<hr></div>
+        <div class="">내 과목 통계<hr></div>
         <el-row type="flex" justify="center">
           <el-col>
-            <div class="grid-content bg-white">ㅎㅎ</div>
+            <div class="grid-content bg-white">통계</div>
           </el-col>
         </el-row>
       </el-main>
@@ -40,17 +38,39 @@
 </template>
 
 <script>
+import classService from '../../services/classService';
+
 export default {
   name: 'Home',
   data() {
     return {
+      popClasses: '',
     };
+  },
+  computed: {
+    width() {
+      window.console.log('width', document.body.scrollWidth);
+      return document.body.scrollWidth;
+    },
+  },
+  async mounted() {
+    const vm = this;
+    vm.popClasses = await classService.popClass();
+    window.console.log(vm.popClasses);
+    window.console.log(vm.width);
   },
 };
 </script>
 
 
 <style scoped>
+  .container {
+    width: 90%;
+    max-width: 1400px;
+    min-width: 700px;
+    margin: auto;
+  }
+
   .el-header, .el-footer {
     background-color: #B3C0D1;
     color: #333;
@@ -74,9 +94,26 @@ export default {
     background-color: rgb(255, 255, 255);
   }
 
+  .bg-film {
+    background-color: #C0C4CC;
+  }
+
+  .film-content {
+    min-height: 150px;
+    padding: 25px;
+    margin-bottom: 30px;
+  }
+
+  .thumbnail-content {
+    min-height: 200px;
+    width: 600px;
+    padding: 30px;
+    margin-bottom: 30px;
+  }
+
   .grid-content {
     border-radius: 4px;
-    min-height: 300px;
+    min-height: 200px;
     padding: 25px;
     margin-bottom: 30px;
   }
--- src/services/classService.js
+++ src/services/classService.js
@@ -41,4 +41,34 @@ export default {
       }, 500);
     });
   },
+  popClass() {
+    // 인기강의 정보
+    console.log('Fetch classes info. which are popular than others'); // eslint-disable-line
+
+    // TODO: Replace dummy with axios
+    return new Promise((resolve) => {
+      window.setTimeout(() => {
+        resolve({
+          class1: {
+            className: 'Python',
+          },
+          class2: {
+            className: 'JavaScript',
+          },
+          class3: {
+            className: 'C++',
+          },
+          class4: {
+            className: 'Vue.js',
+          },
+          class5: {
+            className: 'Node.js',
+          },
+          class6: {
+            className: 'Deep learning',
+          },
+        });
+      }, 500);
+    });
+  },
 };
