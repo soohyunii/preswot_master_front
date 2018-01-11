08e244af411f841d9183b711e8cf412b041fd7d4
Olaf Ahn <adoji92@gmail.com>
2018-01-11 16:37:38 +0900

    Add TODOs @ TeacherHome.vue

--- src/components/pages/TeacherHome.vue
+++ src/components/pages/TeacherHome.vue
@@ -2,18 +2,23 @@
   <div>
     <el-container class="outline">
       <el-aside width="150px">
+        <!-- TODO: translation -->
         <el-row>
           파이썬 초급
         </el-row>
+        <!-- TODO: translation -->
+
         <el-row>
           자바
         </el-row>
         <el-row>
           <!-- TODO: Implement adding lecture part -->
+          <!-- TODO: translation -->
           <el-button>과목 추가</el-button>
         </el-row>
       </el-aside>
       <el-main>
+        <!-- TODO: translation -->
         <h3>자바</h3><hr>
         <el-button>과목지식맵</el-button>
         <el-button>과목큐레이션</el-button>
@@ -22,27 +27,32 @@
         <el-row :gutter="20">
           <el-col :span="16">
             <div class="grid-content bg-white">
+              <!-- TODO: translation -->
               강의 시나리오 목록<hr>
               <el-table
                 :data="tableData"
                 style="width: 100%">
+                <!-- TODO: translation -->
                 <el-table-column
                   prop="num"
                   label="번호"
                   sortable
                   width="180">
                 </el-table-column>
+                <!-- TODO: translation -->
                 <el-table-column
                   prop="name"
                   label="강의 시나리오"
                   sortable
                   width="180">
                 </el-table-column>
+                <!-- TODO: translation -->
                 <el-table-column
                   prop="date"
                   label="강의 날짜"
                   sortable>
                 </el-table-column>
+                <!-- TODO: translation -->
                 <el-table-column
                   prop="understanding"
                   label="수강생 이해도(평균)"
@@ -73,12 +83,14 @@
               </el-table>
               <router-link to="/a/teacher/lecture/new">
                 <br /><br />
+                <!-- TODO: translation -->
                 <el-button>강의 시나리오 추가</el-button>
               </router-link>
             </div>
           </el-col>
           <el-col :span="8">
             <div class="grid-content bg-white">
+              <!-- TODO: translation -->
               <el-col :span="16">수강생 목록</el-col>
               <el-col :span="8" style="text-align: right;">8명</el-col>
               <hr>
@@ -87,6 +99,7 @@
         </el-row>
         <el-row :gutter="20">
           <el-col>
+            <!-- TODO: translation -->
             <div class="grid-content bg-white">과목 통계<hr></div>
           </el-col>
         </el-row>
