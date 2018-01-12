7cb8c1ebd8c3457be591bdef773a0c8ce69ea541
이민현 <lmh315113@gmail.com>
2018-01-12 15:52:37 +0900

    Implement StudentRealtime.vue layout by hardcoding

---
+++ src/components/pages/StudentRealtime.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <el-container>
+            <el-aside width="100px"></el-aside>
+            <el-main>
+                <!-- TODO: translation -->
+                자바 4강(배열)<hr>
+                <el-row>
+                    <el-col :span="12">
+                        <div>영상이 들어가는 공간</div>
+                    </el-col>
+                    <el-col :span="12">
+                       <student-realtime-summary></student-realtime-summary>
+                    </el-col>
+                </el-row>
+                <el-row>
+                    <el-col :span="12">
+                        <div>문제가 들어가는 공간<hr></div>
+                    </el-col>
+                    <el-col :span="12">
+                        <div>강의자료<hr></div>
+                    </el-col>
+                </el-row>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<script>
+import StudentRealtimeSummary from '../partial/StudentRealtimeSummary';
+
+export default {
+  name: 'StudentRealtime',
+  components: {
+    StudentRealtimeSummary,
+  },
+};
+</script>
