c6b96bf3d6b7d4cba27214818a0da36e4b83be09
이민현 <lmh315113@gmail.com>
2018-01-12 16:29:39 +0900

    Add temporary close button

--- src/components/pages/StudentRealtime.vue
+++ src/components/pages/StudentRealtime.vue
@@ -6,18 +6,26 @@
                 <!-- TODO: translation -->
                 자바 4강(배열)<hr>
                 <el-row>
-                    <el-col :span="12">
-                        <div>영상이 들어가는 공간</div>
+                    <el-col :span="12" v-if="!isClose">
+                        <!-- TODO: translation -->
+                        <div>
+                            영상이 들어가는 공간(팝업버튼)
+                            <el-button @click="onClick('CLOSE')">X</el-button>
+                        </div>
                     </el-col>
-                    <el-col :span="12">
-                       <student-realtime-summary></student-realtime-summary>
+                    <el-col :span="isClose ? 24 : 12">
+                        <!-- TODO: Get SummaryData from dummy -->
+                       <student-realtime-summary :SummaryData="SummaryData"></student-realtime-summary>
                     </el-col>
                 </el-row>
+                <br /><br />
                 <el-row>
                     <el-col :span="12">
+                        <!-- TODO: translation -->
                         <div>문제가 들어가는 공간<hr></div>
                     </el-col>
                     <el-col :span="12">
+                        <!-- TODO: translation -->
                         <div>강의자료<hr></div>
                     </el-col>
                 </el-row>
@@ -33,6 +41,26 @@ export default {
   name: 'StudentRealtime',
   components: {
     StudentRealtimeSummary,
+  },
+  data() {
+    return {
+      isClose: false,
+      SummaryData: [],
+    };
+  },
+  methods: {
+    onClick(type) {
+      const vm = this;
+      switch (type) {
+        case 'CLOSE': {
+          vm.isClose = true;
+          break;
+        }
+        default: {
+          throw new Error('not defined type', type);
+        }
+      }
+    },
   },
 };
 </script>
