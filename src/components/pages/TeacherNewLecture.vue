9be43be80635b951390f6bb2ed7d72aff843956a
Olaf Ahn <adoji92@gmail.com>
2018-01-12 10:04:29 +0900

    Remove unexpected css / Remove unexpected usage of el-main, header, footer @ TeacherNewLecture.vue

--- src/components/pages/TeacherNewLecture.vue
+++ src/components/pages/TeacherNewLecture.vue
@@ -7,101 +7,58 @@
           <div>강의중인 과목 목록</div>
         </el-row>
       </el-aside>
-      <el-main class="main-box">
-        <el-header>
-          <el-row>
-            <!-- TODO: translation -->
-            <el-col :span="24"><div class="lecture-number-name">4강 (배열)</div></el-col>
-          </el-row>
-        </el-header>
+      <!-- 이 메인은 맞음 시작 -->
+      <el-main>
+        <el-row>
+          <!-- TODO: translation -->
+          <el-col :span="24"><div class="lecture-number-name">4강 (배열)</div></el-col>
+        </el-row>
 
         <el-row :gutter="30">
           <el-col :span="16">
             <!-- TODO: translation -->
-            <div class="scenario bg-purple">시퀀스 들이 배열되는 공간</div>
+            <div>시퀀스 들이 배열되는 공간</div>
           </el-col>
           <el-col :span="8">
-            <div class="scenario bg-purple-light sequence-method">
-              <el-main>
-                <div class="method-list"></div>
-              </el-main>
-              <el-footer>
-                <!-- TODO: translation -->
-                시퀀스 제작 도구
-              </el-footer>
-            </div>
+            <lecture-element-group />
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="16">
-            <div class="item-config-box bg-purple-dark">
-              <el-header>
-                <!-- TODO: translation -->
-                <div class="lecture-number-name">새 문항 입력하기</div>
-              </el-header>
-              <el-main>
-                <!-- TODO: translation -->
-                <div>문항에 관련된 사항 입력 공간</div>
-              </el-main>
-            </div>
+            <!-- TODO: translation -->
+            <div class="lecture-number-name">새 문항 입력하기</div>
+            <div>문항에 관련된 사항 입력 공간</div>
           </el-col>
           <el-col :span="8">
-            <div class="item-config-box bg-purple-dark">
-              <el-header>
-                <!-- TODO: translation -->
-                <div class="lecture-number-name">활성화 방식</div>
-              </el-header>
-              <el-main>
-                  <div></div>
-              </el-main>
+            <div>
+              <!-- TODO: translation -->
+              <div class="lecture-number-name">활성화 방식</div>
             </div>
           </el-col>
         </el-row>
       </el-main>
+      <!-- 이 메인은 맞음 끝 -->
     </el-container>
 
   </div>
 </template>
 
+<script>
+import LectureElementGroup from '../partials/LectureElementGroup';
+
+export default {
+  name: 'TeacherNewLecture',
+  components: {
+    LectureElementGroup,
+  },
+}
+</script>
+
+
 <style scoped>
-  .method-list {
-    min-height: 100px;
-  }
   .lecture-number-name {
     border-bottom-style: solid;
     border-bottom-width: 1px;
     border-bottom-color: darkorange;
-  }
-  .main-box {
-    background: #dedfe0;
-  }
-  .el-row {
-    margin-bottom: 20px;
-  }
-  .scenario {
-    min-height: 150px;
-  }
-  .sequence-method {
-    border-style: dashed;
-  }
-  .bg-purple-dark {
-    background: #9aabc2;
-  }
-  .subject-list {
-    width: 20px;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .item-config-box {
-    min-height: 400px;
-    padding-top: 15px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
   }
 </style>
