01e58d41b9f2c6a0156b85d8f10d8485de8f30bf
Olaf Ahn <adoji92@gmail.com>
2018-01-12 10:26:42 +0900

    Inject lectureElementSequence from teacherStore to LectureElementSequence.vue

--- src/components/partials/LectureElementSequence.vue
+++ src/components/partials/LectureElementSequence.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    Lecture Element Sequence Template
+    Lecture Element Sequence Template <br />
+    seq variable: {{  lectureElementSequence}}
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'LectureElementSequnce',
+  computed: {
+    ...mapState('teacher', ['lectureElementSequence']),
+  },
 };
 </script>
