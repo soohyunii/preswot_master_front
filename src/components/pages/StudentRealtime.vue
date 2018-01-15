727e3e042512c4c5d6202e1eaee0c0cc8c69bba8
이민현 <lmh315113@gmail.com>
2018-01-15 17:05:22 +0900

    Update StudentRealtime.vue footer understanding bar

--- src/components/pages/StudentRealtime.vue
+++ src/components/pages/StudentRealtime.vue
@@ -6,28 +6,36 @@
                 <!-- TODO: translation -->
                 자바 4강(배열)<hr>
                 <el-row>
-                    <el-col :span="12" v-if="!isClose">
+                    <el-col :span="24" v-if="!isCloseMovie">
                         <!-- TODO: translation -->
                         <div>
                             영상이 들어가는 공간(팝업버튼)
-                            <el-button @click="onClick('CLOSE')">X</el-button>
+                            <el-button @click="onClick('CLOSE_MOVIE')">X</el-button>
                         </div>
-                    </el-col>
-                    <el-col :span="isClose ? 24 : 12">
-                        <!-- TODO: Get SummaryData from dummy -->
-                       <student-realtime-summary :SummaryData="SummaryData"></student-realtime-summary>
                     </el-col>
                 </el-row>
                 <br /><br />
                 <el-row>
-                    <el-col :span="12">
+                    <el-col :span="isCloseMovie ? 24 : 12">
                         <!-- TODO: translation -->
                         <div>문제가 들어가는 공간<hr></div>
                     </el-col>
                     <el-col :span="12">
                         <!-- TODO: translation -->
+                        <div>설문이 들어가는 공간<hr></div>
+                    </el-col>
+                    <el-col :span="12">
+                        <!-- TODO: translation -->
                         <div>강의자료<hr></div>
                     </el-col>
+                </el-row>
+                <el-row>
+                    <div id="footer" v-if="!isCloseFooter">
+                        <student-realtime-summary :SummaryData="SummaryData"></student-realtime-summary>
+                        <el-col :span="5" style="text-align:right;">
+                            <i class="el-icon-close" @click="onClick('CLOSE_FOOTER')"></i>
+                        </el-col>
+                    </div>
                 </el-row>
             </el-main>
         </el-container>
@@ -44,7 +52,8 @@ export default {
   },
   data() {
     return {
-      isClose: false,
+      isCloseMovie: false,
+      isCloseFooter: false,
       SummaryData: [],
     };
   },
@@ -52,8 +61,12 @@ export default {
     onClick(type) {
       const vm = this;
       switch (type) {
-        case 'CLOSE': {
-          vm.isClose = true;
+        case 'CLOSE_MOVIE': {
+          vm.isCloseMovie = true;
+          break;
+        }
+        case 'CLOSE_FOOTER': {
+          vm.isCloseFooter = true;
           break;
         }
         default: {
@@ -64,3 +77,15 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#footer {
+   position:fixed;
+   left:0px;
+   bottom:0px;
+   height: 40px;
+   width:100%;
+   background:rgba(0, 0, 0, 0.5);
+   color: white;
+}
+</style>
