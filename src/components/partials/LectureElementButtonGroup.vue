ac6cb2231f5e5968d074c570db36f35a079268fb
LSB <tmdqhr2586@naver.com>
2018-01-15 14:40:04 +0900

    Add lecture name edit icon & function / Edit layout of TeacherNewLecture

--- src/components/partials/LectureElementButtonGroup.vue
+++ src/components/partials/LectureElementButtonGroup.vue
@@ -1,10 +1,45 @@
 <template>
   <div>
     <h1>Lecture Element Group template</h1>
-    <el-button @click="onClick('A')">Type A</el-button>
-    <el-button @click="onClick('B')">Type B</el-button>
-    <el-button @click="onClick('C')">Type C</el-button>
-    <el-button @click="onClick('D')">Type D</el-button>
+    <el-row :gutter="10">
+      <el-col align="center" :span="6">
+        <!-- TODO: Translate, change icon -->
+        <el-button @click="onClick('A')">
+          <i class="el-icon-question" style="font-size: 50px;"></i><br/>문항설문
+        </el-button>
+      </el-col>
+      <el-col align="center" :span="6">
+        <!-- TODO: Translate, change icon -->
+        <el-button @click="onClick('B')">
+          <i class="el-icon-info" style="font-size: 50px;"></i><br/>강의자료
+        </el-button>
+      </el-col>
+      <el-col align="center" :span="6">
+        <!-- TODO: Translate, change icon -->
+        <el-button @click="onClick('C')">
+          <i class="el-icon-document" style="font-size: 50px;"></i><br/>숙제
+        </el-button>
+      </el-col>
+      <el-col align="center" :span="6">
+        <!-- TODO: Translate, change icon -->
+        <el-button @click="onClick('D')">
+          <i class="el-icon-setting" style="font-size: 50px;"></i><br/>코딩
+        </el-button>
+      </el-col>
+    </el-row>
+    <br/>
+    <el-row>
+      <el-col :span="8">
+        <hr/>
+      </el-col>
+      <!-- TODO: Translate -->
+      <el-col :span="8" align="center">
+        시퀀스 저작 도구
+      </el-col>
+      <el-col :span="8">
+        <hr/>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
