523c2a31d1dbdb2f43a59458807ebd78fc3c9f73
LSB <tmdqhr2586@naver.com>
2018-01-16 10:16:39 +0900

    Add dummy function fetching class list in el-aside

--- src/components/pages/TeacherHome.vue
+++ src/components/pages/TeacherHome.vue
@@ -1,44 +1,52 @@
 <template>
   <div>
     <el-container class="outline">
+
+      <!-- TODO: 이 부분도 TeacherLectureNew.vue와 동일한데, 독립적으로 분리시키는건 어떨까요? -->
       <el-aside width="150px">
-        <!-- TODO: Implement dummy response about class list -->
-        <!-- TODO: translation -->
-        <el-row>
-          파이썬 초급
-        </el-row>
-        <!-- TODO: translation -->
+        <template v-for="(item, key) in teachingClassList">
+          <el-row :key="key">
+            <el-col align="center">
+              <!-- TODO: link to each class -->
+              <el-button @click="dummy(item)" v-if="disabledummy(item)" type="info" disabled>
+                <i :class="dummyIcons()" style="font-size: 50px;"></i><br/>
+                {{ item.className | truncate(10) }}
+              </el-button>
+              <el-button @click="dummy(item)" v-else>
+                <i :class="dummyIcons()" style="font-size: 50px;"></i><br/>
+                {{ item.className | truncate(10) }}
+              </el-button>
+            </el-col>
+          </el-row>
+        </template>
 
         <el-row>
-          자바
-        </el-row>
-        <el-row>
+          <el-col align="center">
           <!-- TODO: Implement adding lecture part -->
           <!-- TODO: translation -->
-          <el-button>과목 추가</el-button>
+          <el-button @click="dummy('과목추가')">
+            <i class='el-icon-circle-plus' style="font-size: 50px;"></i><br/>
+            과목 추가
+          </el-button>
+          </el-col>
         </el-row>
       </el-aside>
+
       <el-main>
+        <h3>{{currentLecture}}</h3><hr>
+
         <!-- TODO: translation -->
-        <h3>자바</h3><hr>
+        <!-- TODO: Link to each page per button-->
         <el-button>과목지식맵</el-button>
         <el-button>과목큐레이션</el-button>
         <el-button>과목저널링</el-button>
         <br /><br />
-        <el-row :gutter="20">
-          <!-- TODO: Implement dummy response about lecture list -->
-          <lecture-list :tableData="tableData"></lecture-list>
-          <el-col :span="8">
-            <div class="grid-content bg-white">
-              <!-- TODO: translation -->
-              <el-col :span="16">수강생 목록</el-col>
-              <el-col :span="8" style="text-align: right;">8명</el-col>
-              <hr>
-            </div>
-          </el-col>
-        </el-row>
-          <!-- TODO: Implement dummy response about lecture statistics -->
-          <lecture-statistics></lecture-statistics>
+
+        <!-- TODO: Implement dummy response about lecture list -->
+        <lecture-list :tableData="tableData"></lecture-list>
+
+        <!-- TODO: Implement dummy response about lecture statistics -->
+        <lecture-statistics></lecture-statistics>
       </el-main>
     </el-container>
   </div>
@@ -47,6 +55,7 @@
 <script>
 import LectureList from '../partials/LectureList';
 import LectureStatistics from '../partials/LectureStatistics';
+import classService from '../../services/classService';
 
 export default {
   name: 'TeacherHome',
@@ -73,7 +82,33 @@ export default {
         name: '4강',
         understanding: 72,
       }],
+      currentLecture: '',
+      teachingClassList: [],
     };
+  },
+  methods: {
+    dummyIcons() {
+      return 'el-icon-document';
+    },
+    dummy(index) {
+      window.console.log(index);
+    },
+    disabledummy(item) {
+      if (item.className === 'Vue.js') {
+        const vm = this;
+        vm.currentLecture = item.className;
+        return true;
+      }
+      return false;
+    },
+    changeLectureName() {
+      const vm = this;
+      vm.inputFlag = !vm.inputFlag;
+    },
+  },
+  async mounted() {
+    const vm = this;
+    vm.teachingClassList = await classService.fetchTeachingClassList();
   },
   components: {
     LectureList,
--- src/components/partials/LectureList.vue
+++ src/components/partials/LectureList.vue
@@ -1,61 +1,63 @@
 <template>
-    <el-col :span="16">
-    <div class="grid-content bg-white">
-        강의 시나리오 목록<hr>
-        <el-table
-        :data="tableData"
-        style="width: 100%">
-        <el-table-column
-            prop="num"
-            label="번호"
-            sortable
-            width="180">
-        </el-table-column>
-        <el-table-column
-            prop="name"
-            label="강의 시나리오"
-            sortable
-            width="180">
-        </el-table-column>
-        <el-table-column
-            prop="date"
-            label="강의 날짜"
-            sortable>
-        </el-table-column>
-        <el-table-column
-            prop="understanding"
-            label="수강생 이해도(평균)"
-            sortable>
-            <template slot-scope="scope">
-            <el-progress
-                v-if="scope.row.understanding === 100"
-                :text-inside="true"
-                :stroke-width="18"
-                :percentage="scope.row.understanding"
-                status="success">
-            </el-progress>
-            <el-progress
-                v-else-if="scope.row.understanding > 70"
-                :text-inside="true"
-                :stroke-width="18"
-                :percentage="scope.row.understanding">
-            </el-progress>
-            <el-progress
-                v-else
-                :text-inside="true"
-                :stroke-width="18"
-                :percentage="scope.row.understanding"
-                status="exception">
-            </el-progress>
-            </template>
-        </el-table-column>
-        </el-table>
-        <router-link to="/a/teacher/lecture/new">
-        <br /><br />
-        <el-button>강의 시나리오 추가</el-button>
-        </router-link>
-    </div>
-    </el-col>
+    <el-row :gutter="20">
+        <el-col>
+        <div class="grid-content bg-white">
+            강의 시나리오 목록<hr>
+            <el-table
+            :data="tableData"
+            style="width: 100%">
+            <el-table-column
+                prop="num"
+                label="번호"
+                sortable
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="name"
+                label="강의 시나리오"
+                sortable
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="date"
+                label="강의 날짜"
+                sortable>
+            </el-table-column>
+            <el-table-column
+                prop="understanding"
+                label="수강생 이해도(평균)"
+                sortable>
+                <template slot-scope="scope">
+                <el-progress
+                    v-if="scope.row.understanding === 100"
+                    :text-inside="true"
+                    :stroke-width="18"
+                    :percentage="scope.row.understanding"
+                    status="success">
+                </el-progress>
+                <el-progress
+                    v-else-if="scope.row.understanding > 70"
+                    :text-inside="true"
+                    :stroke-width="18"
+                    :percentage="scope.row.understanding">
+                </el-progress>
+                <el-progress
+                    v-else
+                    :text-inside="true"
+                    :stroke-width="18"
+                    :percentage="scope.row.understanding"
+                    status="exception">
+                </el-progress>
+                </template>
+            </el-table-column>
+            </el-table>
+            <router-link to="/a/teacher/lecture/new">
+            <br /><br />
+            <el-button>강의 시나리오 추가</el-button>
+            </router-link>
+        </div>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
