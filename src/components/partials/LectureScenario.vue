23f34dfcb5687e4213e22f758be2f311e3d22fb8
신지호 <supugum@gmail.com>
2018-01-17 17:46:08 +0900

    Extract draggable tag from LectureScenarioitem.vue to LectureScenario.vue not complete

--- src/components/partials/LectureScenario.vue
+++ src/components/partials/LectureScenario.vue
@@ -2,16 +2,36 @@
   <div>
     <h1>Lecture Element Sequence Template</h1>
     <el-row :gutter="10">
-      <lecture-scenario-item></lecture-scenario-item>
+      <draggable v-model="lectureElementSequence" @start="drag=true" @end="drag=false">
+        <lecture-scenario-item :lectureElementSequence="lectureElementSequence"></lecture-scenario-item>
+      </draggable>
     </el-row>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable';
 import LectureScenarioItem from './LectureScenarioItem';
 
 export default {
+  data() {
+    return {
+      drag: false,
+    };
+  },
+  computed: {
+    lectureElementSequence: {
+      get() {
+        return this.$store.state.teacher.lectureElementSequence;
+      },
+      set(value) {
+        window.console.log('dasdada');
+        this.$store.commit('teacher/editLectureElement', { evt: value });
+      },
+    },
+  },
   components: {
+    draggable,
     LectureScenarioItem,
   },
 };
--- src/components/partials/LectureScenarioItem.vue
+++ src/components/partials/LectureScenarioItem.vue
@@ -1,50 +1,32 @@
 <template>
+  <!-- TODO: <div> 태그로 인해 드래그가 먹지 않음-->
   <div>
-    <draggable v-model="lectureElementSequence" @start="drag=true" @end="drag=false">
-      <template v-for="(item, index, key) in lectureElementSequence">
-        <el-col :span="3" :key="key" align="center">
-          <!-- TODO: change icons -->
-          <!-- TODO: Translate tooltip -->
-          <i :class="getIconsByType(item.type)" style="font-size: 50px;"></i>
-          <i class="el-icon-error" style="color:red; vertical-align:top" @click="onClick(index)"></i><br/>
-          <!-- TODO: change bg color, time variable -->
-          <el-tag color="#DCDFE6">time</el-tag><br/>
+    <template v-for="(item, index, key) in lectureElementSequence">
+      <el-col :span="3" :key="key" align="center">
+        <!-- TODO: change icons -->
+        <!-- TODO: Translate tooltip -->
+        <i :class="getIconsByType(item.type)" style="font-size: 50px;"></i>
+        <i class="el-icon-error" style="color:red; vertical-align:top" @click="onClick(index)"></i><br/>
+        <!-- TODO: change bg color, time variable -->
+        <el-tag color="#DCDFE6">time</el-tag><br/>
 
-          <!-- TODO: change bg color, duration variable -->
-          <el-tag color="#F2F6FC">duration</el-tag><br/>
-        </el-col>
-      </template>    
-    </draggable>
-    
+        <!-- TODO: change bg color, duration variable -->
+        <el-tag color="#F2F6FC">duration</el-tag><br/>
+      </el-col>
+    </template>
   </div>
 </template>
 
 <script>
 import { mapMutations } from 'vuex';
-import draggable from 'vuedraggable';
 
 export default {
-  data() {
-    return {
-      drag: false,
-    };
-  },
   name: 'LectureScenario',
-  computed: {
-    lectureElementSequence: {
-      get() {
-        return this.$store.state.teacher.lectureElementSequence;
-      },
-      set(value) {
-        this.$store.commit('teacher/editLectureElement', { evt: value });
-      },
-    },
-  },
+  props: ['lectureElementSequence'],
   methods: {
     // TODO: edit lecture element
     // TODO: add drag/drop function
     ...mapMutations('teacher', ['deleteLectureElement']),
-    ...mapMutations('teacher', ['editLectureElement']),
     onClick(index) {
       const vm = this;
       if (index < Object.keys(vm.lectureElementSequence).length) {
@@ -77,9 +59,6 @@ export default {
       }
       return icon;
     },
-  },
-  components: {
-    draggable,
   },
 };
 </script>
