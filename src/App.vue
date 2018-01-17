3473954f0d8192d5fe67b8764704c96997fea850
Olaf Ahn <adoji92@gmail.com>
2018-01-17 10:19:54 +0900

    Fix isNavCollapsed logical error

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <app-header />
       </el-header>
       <el-container>
-        <el-aside width="200px" v-show="isNavCollapsed">
+        <el-aside width="200px" v-show="!isNavCollapsed">
           <app-navigation />
         </el-aside>
         <el-container>
