6605cec73c96dbf11692de1773c5615cae788d0d
이민현 <lmh315113@gmail.com>
2018-01-18 10:58:24 +0900

    Merge tooltips in teachingClassList.vue into AppNavigaion.vue

--- src/components/layouts/AppNavigation.vue
+++ src/components/layouts/AppNavigation.vue
@@ -29,11 +29,15 @@
         </template>
         <el-menu-item-group>
           <template v-for="(item, key, index) in attendingClassList">
-            <el-tooltip effect="dark" placement="right" :key="key">
-              <div slot="content">{{ item.className }}</div>
+            <el-tooltip
+              effect="dark"
+              :content="item.className"
+              :disabled="item.className.length < truncateLength"
+              placement="right"
+              :key="key">
               <el-menu-item :index="'3-'+index" :key="key">
                 <!-- TODO: link to each class -->
-                {{ item.className | truncate(16) }}
+                {{ item.className | truncate(truncateLength) }}
               </el-menu-item>
             </el-tooltip>
           </template>
@@ -48,11 +52,15 @@
         </template>
         <el-menu-item-group>
           <template v-for="(item, key, index) in teachingClassList">
-            <el-tooltip effect="dark" placement="right" :key="key">
-              <div slot="content">{{ item.className }}</div>
+            <el-tooltip
+              effect="dark"
+              :content="item.className"
+              :disabled="item.className.length < truncateLength"
+              placement="right"
+              :key="key">
               <el-menu-item :index="'4-'+index" :key="key">
                 <!-- TODO: link to each class -->
-                {{ item.className | truncate(16) }}
+                {{ item.className | truncate(truncateLength) }}
               </el-menu-item>
             </el-tooltip>
           </template>
@@ -75,6 +83,7 @@ export default {
   data() {
     return {
       // isCollapse: false,
+      truncateLength: 16,
       attendingClassList: [],
       teachingClassList: [],
     };
@@ -82,28 +91,15 @@ export default {
   computed: {
     ...mapState('layout', ['isNavCollapsed']),
   },
-  mounted() {
+  async mounted() {
     const vm = this;
-    studentService.fetchAttendingClassList()
-    .then((attendingClassList) => {
-      vm.attendingClassList = attendingClassList;
-    })
-    .catch(() => {
-      // fetch fail
-    });
-
-    teacherService.fetchTeachingClassList()
-    .then((teachingClassList) => {
-      vm.teachingClassList = teachingClassList;
-    })
-    .catch(() => {
-      // fetch fail
-    });
+    vm.attendingClassList = await studentService.fetchAttendingClassList();
+    vm.teachingClassList = await teacherService.fetchTeachingClassList();
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 // * handling font awesome icons
 #app_nav_wrapper {
   height: 100%;
