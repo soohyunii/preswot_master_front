0ea5a5e5c16ebd31b39679d4dc54b880f818451b
Olaf Ahn <adoji92@gmail.com>
2018-01-22 17:24:10 +0900

    Add currentEditingLectureScenarioItem @ teacherSTore state

--- src/stores/teacherStore.js
+++ src/stores/teacherStore.js
@@ -5,7 +5,8 @@ export default {
   state: {
     lectureScenario: [], // TODO: fetch(init) from server if exists
     // TODO: Otherwise, init from localStorage (in case of server has no data)
-    currentEditingElement: null, // TODO: init from localStorage
+    // currentEditingLectureScenarioItem: 현재 저작 중인 강의 시나리오 아이템
+    currentEditingLectureScenarioItem: null, // TODO: init from localStorage
     // TODO: save currentEditingElement into localStorage inside Vue component using watch
     teachingClassList: [],
     currentEditingClass: null,
@@ -19,8 +20,13 @@ export default {
   mutations: {
     pushLectureScenarioItem(state, { type }) {
       const key = Guid.create().toString();
-      state.lectureScenario.push({ type, key });
+      const lectureScenarioItem = { type, key };
+      state.currentEditingLectureScenarioItem = lectureScenarioItem;
+      state.lectureScenario.push(lectureScenarioItem);
       // TODO: save lectureElementSequence using localForage
+    },
+    updateCurrentEditingLectureScenarioItem(state, { currentEditingLectureScenarioItem }) {
+      state.currentEditingLectureScenarioItem = currentEditingLectureScenarioItem;
     },
     deleteLectureScenarioItem(state, { lectureElementIndex }) {
       state.lectureScenario.splice(lectureElementIndex, 1);
