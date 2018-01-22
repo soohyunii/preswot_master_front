ec37297a5183b92ad2f2c6d10f08c58e480527f9
Olaf Ahn <adoji92@gmail.com>
2018-01-22 14:21:13 +0900

    Better main height computing @ LectureScenario

--- src/components/partials/LectureScenario.vue
+++ src/components/partials/LectureScenario.vue
@@ -7,21 +7,28 @@
       </el-aside>
       <el-main>
         <div ref="main">
-          <el-row :gutter="10">
-            <!-- TODO: middle bar between two Items -->
-            <draggable v-model="lectureScenario"
-              :options="dragOptions"
-              @start="drag = true;"
-              @end="drag = false;">
-              <transition-group name="list-group">
-                <lecture-scenario-item
-                  v-for="(item, index) in lectureScenario"
-                  class="list-group-item"
-                  :key="item.key"
-                  :props="{ item, index }" />
-              </transition-group>
-            </draggable>
-          </el-row>
+          <div v-show="!lectureScenario || lectureScenario.length === 0">
+            <!-- TODO: translation -->
+            <!-- TODO: styling -->
+            Empty Scenario
+          </div>
+          <div v-show="!!lectureScenario && lectureScenario.length !== 0">
+            <el-row :gutter="10">
+              <!-- TODO: middle bar between two Items -->
+              <draggable v-model="lectureScenario"
+                :options="dragOptions"
+                @start="drag = true;"
+                @end="drag = false;">
+                <transition-group name="list-group">
+                  <lecture-scenario-item
+                    v-for="(item, index) in lectureScenario"
+                    class="list-group-item"
+                    :key="item.key"
+                    :props="{ item, index }" />
+                </transition-group>
+              </draggable>
+            </el-row>
+          </div>
         </div>
       </el-main>
     </el-container>
@@ -113,28 +120,13 @@ export default {
     },
   },
   methods: {
-    getLabelStyle() {
-      const main = this.$refs.main;
-      const res = {};
-      if (!main) {
-        res.height = '200px';
-      } else {
-        res.height = `${main.clientHeight + 40}px`;
-      }
-      // this.$nextTick(() => {
-      //   this.$forceUpdate();
-      // });
-      return res;
-    },
     updateLabelStyle() {
       const vm = this;
       const main = this.$refs.main;
-      if (!main) {
-        vm.labelStyle.height = '200px';
-      } else if (main.clientHeight !== 0) {
-        vm.labelStyle.height = `${main.clientHeight + 40}px`;
-      } else {
+      if (!vm.lectureScenario || vm.lectureScenario.length === 0) {
         vm.labelStyle.height = '154px';
+      } else {
+        vm.labelStyle.height = `${main.clientHeight + 40}px`;
       }
       return vm.labelStyle;
     },
