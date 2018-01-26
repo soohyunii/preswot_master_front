a501462e4998e3f1761413585a0d6547a9c26c04
Olaf Ahn <adoji92@gmail.com>
2018-01-26 11:29:20 +0900

    Create currentTeachingScItemIndex @ teacherStore.state

--- src/stores/teacherStore.js
+++ src/stores/teacherStore.js
@@ -3,15 +3,17 @@ const Guid = require('guid');
 export default {
   namespaced: true,
   /**
-   * @var Sc: shorthand for Scenario
+   * @var sc: shorthand for Scenario
+   * @var currentEditingScItemIndex: 현재 생성/편집 중인 시나리오 아이템 인덱스
+   * @var currentTeachingScItemIndex: 강의 중에 현재 진행되고 있는 시나리오 아이템 인덱스
    */
   state: {
     sc: [], // TODO: fetch(init) from server if exists
     // TODO: Otherwise, init from localStorage (in case of server has no data)
-    // currentEditingScItem: 현재 저작 중인 시나리오 아이템
     // currentEditingScItem: null, // TODO: init from localStorage
     currentEditingScItemIndex: null,
     // TODO: save currentEditingElement into localStorage inside Vue component using watch
+    currentTeachingScItemIndex: null,
     teachingClassList: [],
     currentEditingClass: null,
     // scenarioList: [],
@@ -24,6 +26,9 @@ export default {
     },
     currentEditingScItem(state) {
       return state.sc[state.currentEditingScItemIndex];
+    },
+    currentTeachingScItem(state) {
+      return state.sc[state.currentTeachingScItemIndex];
     },
   },
   mutations: {
@@ -48,6 +53,9 @@ export default {
           currentEditingScItem,
         );
       }
+    },
+    updateCurrentTeachingScItemIndex(state, { index }) {
+      state.currentTeachingScItemIndex = index;
     },
     deleteScItem(state, { lectureElementIndex }) {
       state.sc.splice(lectureElementIndex, 1);
