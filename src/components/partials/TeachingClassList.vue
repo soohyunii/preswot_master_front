651006f4568d66a9c3f3669f336c6e78a28a1115
Olaf Ahn <adoji92@gmail.com>
2018-01-31 10:07:27 +0900

    Adjust v-for @ TeachingClassList.vue

--- src/components/partials/TeachingClassList.vue
+++ src/components/partials/TeachingClassList.vue
@@ -1,38 +1,37 @@
 <template>
   <el-aside width="150px" id="teacher_class_list_aside">
-    <template v-for="(item, key) in teachingClassList">
-      <el-row :key="key">
-        <el-col align="center">
-          <!-- TODO: link to each class -->
-          <el-tooltip
-            effect="dark"
-            :content="item.className"
-            :disabled="item.className.length < truncateLength"
-            placement="right"
-            :open-delay="800"
-          >
-            <!-- TODO: currentClass 모습, 나머지 class 모습 바꾸기 -->
-            <el-button
-              class="class-btn"
-              @click="changeCurrentClass(item)"
-              :type="isCurrentClass(item) ? 'info' : ''"
-            >
-              <i :class="dummyIcons()" style="font-size: 50px;"></i>
-              <br />
-              {{ item.className | truncate(truncateLength) }}
-            </el-button>
-          </el-tooltip>
-        </el-col>
-      </el-row>
-    </template>
-
     <el-row>
+      <el-col
+        align="center"
+        v-for="(item, index) in teachingClassList"
+        :key="index"
+      >
+        <!-- TODO: link to each class -->
+        <el-tooltip
+          effect="dark"
+          :content="item.className"
+          :disabled="item.className.length < truncateLength"
+          placement="right"
+          :open-delay="800"
+        >
+          <!-- TODO: currentClass 모습, 나머지 class 모습 바꾸기 -->
+          <el-button
+            class="class-btn"
+            @click="changeCurrentClass(item)"
+            :type="isCurrentClass(item) ? 'info' : ''"
+          >
+            <i :class="dummyIcons()" style="font-size: 50px;"></i>
+            <br />
+            {{ item.className | truncate(truncateLength) }}
+          </el-button>
+        </el-tooltip>
+      </el-col>
       <el-col align="center">
         <!-- TODO: Implement adding lecture part -->
         <!-- TODO: translation -->
-        <el-button @click="dummyAddClass()">
-        <i class='el-icon-circle-plus' style="font-size: 50px;"></i> <br/>
-        과목 추가
+        <el-button class="class-btn" @click="dummyAddClass()">
+          <i class='el-icon-circle-plus' style="font-size: 50px;"></i> <br/>
+          과목 추가
         </el-button>
       </el-col>
     </el-row>
@@ -59,7 +58,7 @@ export default {
   name: 'teachingClassList',
   data() {
     return {
-      truncateLength: 5,
+      truncateLength: 10,
     };
   },
   computed: {
