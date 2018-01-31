5191cdcea46e5fccd02aa2f8d5a32f9d1521df83
이민현 <lmh315113@gmail.com>
2018-01-31 15:39:33 +0900

    Implement validation for node name

--- src/components/partials/KnowledgeMapNodeEditor.vue
+++ src/components/partials/KnowledgeMapNodeEditor.vue
@@ -16,8 +16,14 @@
       <el-table-column label="Name" align="center">
         <template slot-scope="scope">
           <div v-if="inputFlag[scope.$index] && inputFlag[scope.$index].value">
-            <el-input v-model="nodes[scope.$index].value" />
-            <el-button @click="onClick('changeNodeName', scope.$index)">확인</el-button>
+            <el-form :model="nodes[scope.$index]" :rules="rules" ref="ruleForm" class="demo-ruleForm">
+              <el-form-item prop="name">
+                <el-input v-model="nodes[scope.$index].value" />
+              </el-form-item>
+              <el-form-item>
+                <el-button @click="onClick('changeNodeName', scope.$index)">확인</el-button>
+              </el-form-item>
+            </el-form>
           </div>
           <div v-else>
             <span>{{ scope.row.value }}<i class="el-icon-edit" @click="onClick('changeNodeName', scope.$index)" /></span>
@@ -56,6 +62,13 @@ export default {
     return {
       inputFlag: [],
       count: 1,
+      rules: {
+        name: {
+          required: true,
+          message: 'required',
+          trigger: 'change, blur',
+        },
+      },
     };
   },
   computed: {
