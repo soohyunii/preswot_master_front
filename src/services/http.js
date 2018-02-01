cb67ee981b8f44cfb2ee67cf3117bf6a9d2deb0a
Olaf Ahn <adoji92@gmail.com>
2018-02-01 15:13:21 +0900

    Activate axiosConfig.baseURL with localhost one

--- src/services/http.js
+++ src/services/http.js
@@ -13,8 +13,8 @@ const axiosConfig = {
 
 const isDev = true; // TODO: replace
 if (isDev) {
-  // axiosConfig.baseURL = 'http://localhost:3000';
-  axiosConfig.baseURL = 'http://165.132.105.33:3000';
+  axiosConfig.baseURL = 'http://localhost:3000';
+  // axiosConfig.baseURL = 'http://165.132.105.33:3000';
 }
 
 const jwt = utils.getJwtFromLocalStorage();
