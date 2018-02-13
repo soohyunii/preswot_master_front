16bfbe8eb99db8318b05b7559efeab6831a8ee28
LSB <tmdqhr2586@naver.com>
2018-02-13 16:20:44 +0900

    Remove unnecessary code

--- src/services/surveyService.js
+++ src/services/surveyService.js
@@ -14,26 +14,12 @@ export default {
     comment,
     type,
     choice,
-    timer,
-    inputDescription,
-    outputDescription,
-    sampleInput,
-    sampleOutput,
-    timeLimit,
-    memoryLimit,
   }) {
     const param = {};
 
     utils.assignIfNotNil(param, { comment });
     utils.assignIfNotNil(param, { type });
     utils.assignIfNotNil(param, { choice });
-    utils.assignIfNotNil(param, { timer });
-    utils.assignIfNotNil(param, { inputDescription });
-    utils.assignIfNotNil(param, { outputDescription });
-    utils.assignIfNotNil(param, { sampleInput });
-    utils.assignIfNotNil(param, { sampleOutput });
-    utils.assignIfNotNil(param, { timeLimit });
-    utils.assignIfNotNil(param, { memoryLimit });
 
     return http.put(`/surveys/${surveyId}`, param);
   },
