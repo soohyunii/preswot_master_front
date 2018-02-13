cb676d16038f530477a53ec9203f7deb809a9df6
Olaf Ahn <adoji92@gmail.com>
2018-02-13 16:41:17 +0900

    Implement surveyService again

---
+++ src/services/surveyService.js
@@ -0,0 +1,38 @@
+import http from './http';
+import utils from '../utils';
+
+export default {
+  postSurvey({
+    lectureItemId,
+  }) {
+    return http.post('/surveys', {
+      lectureItemId,
+    });
+  },
+  putSurvey({
+    surveyId,
+    comment,
+    type,
+    choice,
+  }) {
+    const param = {};
+
+    utils.assignIfNotNil(param, { comment });
+    utils.assignIfNotNil(param, { type });
+    utils.assignIfNotNil(param, { choice });
+
+    return http.put(`/surveys/${surveyId}`, param);
+  },
+  postSurveyFile({
+    surveyId,
+    file,
+  }) {
+    const form = new FormData();
+    form.append('file', file, file.name);
+    return http.post(`/surveys/${surveyId}/file`, form, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  },
+};
