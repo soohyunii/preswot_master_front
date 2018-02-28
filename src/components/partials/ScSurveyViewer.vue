fbe5349d52087dc4e7a13cfe64dfe0d56db05b30
LSB <tmdqhr2586@naver.com>
2018-02-28 14:58:07 +0900

    Implement TODOs in @SSV

--- src/components/partials/ScSurveyViewer.vue
+++ src/components/partials/ScSurveyViewer.vue
@@ -3,9 +3,14 @@
     <el-col style="max-width: 600px;">
       <el-form label-width="120px">
         <el-form-item label="파일 목록">
-          <span class="item-text">
-            TODO: 파일 목록gggg
-          </span>
+          <el-button
+            type="primary"
+            v-for="(item, index) in fileList"
+            :key="item.guid"
+            @click="onClick(index)"
+          >
+            {{ item.name }} <i class="el-icon-download el-icon-right"></i>
+          </el-button>
         </el-form-item>
 
         <el-form-item label="설문 내용">
@@ -15,10 +20,17 @@
         </el-form-item>
 
          <el-form-item label="설문 입력">
+          <el-checkbox-group v-if="sChoice.length > 0" v-model="answerChoice">
+            <template v-for="(choice, key) in sChoice">
+              <el-checkbox :label="choice" :key="key"></el-checkbox>
+            </template>
+          </el-checkbox-group>
+          <el-input v-else v-model="answerString">
+          </el-input>
+          <br/>
           <span class="item-text">
-            TODO: 설문 입력폼 (객관) {{ sChoice }}<br />
-            TODO: 설문 입력폼 (주관)<br />
-
+            {{ answerChoice }}<br />
+            {{ answerString }}
           </span>
         </el-form-item>
       </el-form>
@@ -36,8 +48,20 @@ import utils from '../../utils';
 
 export default {
   name: 'ScSurveyViewer',
+  data() {
+    return {
+      // TODO: 학생 답안 저장위치 바꾸기
+      answerChoice: [],
+      answerString: '',
+    };
+  },
   computed: {
     ...mapGetters('scItem', ['currentEditingScItem']),
+    fileList() {
+      const vm = this;
+      const item = vm.currentEditingScItem;
+      return item.fileList;
+    },
     sType() {
       const vm = this;
       const s = vm.currentEditingScItem.survey;
@@ -46,16 +70,23 @@ export default {
     },
     sComment() {
       const vm = this;
-      const s = vm.currentEditingScItem.comment;
+      const s = vm.currentEditingScItem.survey;
       return s ? s.comment : '';
     },
     sChoice() {
       const vm = this;
       const s = vm.currentEditingScItem.survey;
       if (!s.choice) {
-        return null;
+        return [];
       }
-      return s.choice.join(', ');
+      return s.choice;
+    },
+  },
+  methods: {
+    onClick(index) {
+      const vm = this;
+      const file = vm.fileList[index];
+      utils.downloadFile(file.url, file.name);
     },
   },
 };
