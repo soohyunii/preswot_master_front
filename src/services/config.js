af988ab7df10aadefdc316cc49baf743670fa280
Olaf Ahn <adoji92@gmail.com>
2018-03-08 17:05:12 +0900

    Update aws server ip

--- src/services/config.js
+++ src/services/config.js
@@ -1,15 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 
-// const serverUrl = 'http://localhost';
-let serverUrl = 'http://165.132.105.33';
+let serverUrl = 'http://localhost';
+// let serverUrl = 'http://165.132.105.33';
 
 let backPort = 3000;
 let socketPort = 8001;
 
 // const isDev = true;
-const isTest = document.URL.includes('52.78.131.124');
+const isTest = document.URL.includes('13.124.196.202');
 if (isTest) {
-  serverUrl = '13.125.245.55';
+  serverUrl = '13.124.236.81';
   backPort = 8000;
   socketPort = 8001;
 }
