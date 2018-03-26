c7c54101dc94724fbc2487726ec9020c1575480f
Olaf Ahn <adoji92@gmail.com>
2018-03-26 15:13:01 +0900

    Revert isTest @ config

--- src/services/config.js
+++ src/services/config.js
@@ -6,8 +6,8 @@ let serverUrl = 'http://165.132.105.33';
 let backPort = 8000;
 let socketPort = 8001;
 
-// const isTest = document.URL.includes('13.124.196.202');
-const isTest = true;
+const isTest = document.URL.includes('13.124.196.202');
+// const isTest = true;
 if (isTest) {
   serverUrl = 'http://13.124.236.81';
   backPort = 8000;
