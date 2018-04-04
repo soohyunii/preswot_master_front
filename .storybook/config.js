b7c18f5f1bff7c90ed048bd07737db0ee0ed1f00
Olaf Ahn <adoji92@gmail.com>
2018-04-04 17:21:07 +0900

    Beautify code

--- .storybook/config.js
+++ .storybook/config.js
@@ -13,7 +13,7 @@ function loadStories() {
 const req2 = require.context('../src/components', true, /.vue$/);
 req2.keys().forEach((filename) => {
   const vueComponent = req2(filename);
-  console.log(1111, filename, vueComponent);
+  // console.log(1111, filename, vueComponent);
   const tokens = filename.split('/');
   const shortFilename = tokens[tokens.length - 1].replace('.vue', '');
   Vue.component(kebabCase(shortFilename), vueComponent);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,64 +2,14 @@ const path = require('path');
 const vueLoaderConfig = require('../build/vue-loader.conf');
 const devWebpackConfig = require('/home/adoji/git/dblab-academy-front/build/webpack.dev.conf');
 const utils = require('../build/utils');
-console.log(1);
 
 // const defaultStorybookVueWebpackConfig = require('../node_modules/@storybook/vue/dist/server/config/webpack.config.js').default('./webpack.config');
 
-// console.log('deeee', defaultStorybookVueWebpackConfig);
-// module.exports = devWebpackConfig;
-// function resolve(dir) {
-//   return path.join(__dirname, '..', dir)
-// }
-
 module.exports = (baseConfig, env, defaultConfig) => {
-  console.log('baseConfig', baseConfig);
-  console.log('env', env);
-  console.log('default', defaultConfig);
-  // resolve: {
-  //   alias: {
-  //     // 'vue$': 'vue/dist/vue.esm.js',
-  //     '@': path.resolve(__dirname, '../src'),
-  //   },
-  // },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.vue$/,
-  //       loader: 'vue-loader',
-  //       options: vueLoaderConfig
-  //     },
-  //     {
-  //       test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-  //       loader: 'url-loader',
-  //       options: {
-  //         limit: 10000,
-  //         name: utils.assetsPath('img/[name].[hash:7].[ext]')
-  //       }
-  //     },
-  //     // {
-  //     //   test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-  //     //   loader: 'url-loader',
-  //     //   options: {
-  //     //     limit: 10000,
-  //     //     name: utils.assetsPath('media/[name].[hash:7].[ext]')
-  //     //   }
-  //     // },
-  //     // {
-  //     //   test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-  //     //   loader: 'url-loader',
-  //     //   options: {
-  //     //     limit: 10000,
-  //     //     name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-  //     //   }
-  //     // },
-  //     // {
-  //     //   test: /.s[ac]ss$/,
-  //     //   loaders: ['style', 'css', 'sass'],
-  //     //   include: path.resolve(__dirname, '../src'),
-  //     // },
-  //   ],
-  // },
+  // console.log('baseConfig', baseConfig);
+  // console.log('env', env);
+  // console.log('default', defaultConfig);
+
   defaultConfig.resolve.alias['@'] = path.resolve(__dirname, '../src');
   return defaultConfig;
 };
