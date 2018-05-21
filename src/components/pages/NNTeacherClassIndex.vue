7c72e1cecc52a9aa1f09025b8a6bc990275e5831
Dong-jin Ahn <adoji92@gmail.com>
2018-05-21 13:54:42 +0900

    Implement ClassList

--- src/components/pages/NNTeacherClassIndex.vue
+++ src/components/pages/NNTeacherClassIndex.vue
@@ -1,15 +1,54 @@
 <template>
-  <div id="teacher_class_index_wrwapper" class="bt-container">
-    TODO: 과목 목록을 보여준다 쮸쀼쮸쀼 <br />
-    <class-list type="TEACHER" />
-    번호 / 과목 / 과목소개 / 기간 / - / - <br />
-    1 / 한국사 / 누가 국사 소리를 내었는가 / 어제부터 내일까지 / 수정 버튼 / 삭제 버튼 <br />
+  <div id="teacher_class_index_wrapper" class="bt-container">
+    <class-list type="TEACHER" :list="[
+            {
+              class_id: 1,
+              name: 'asdf',
+              description: 'sdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf',
+              intended_lecture_num: 0,
+              start_time: '2018-05-02T06:03:05.000Z',
+              end_time: null,
+              opened: 1,
+              created_at: '2018-05-15T06:03:06.000Z',
+              updated_at: '2018-05-15T07:12:08.000Z',
+              master_id: 11,
+              user_class: {
+                role: 'teacher',
+                class_id: 1,
+                user_id: 11,
+              },
+            },
+            {
+              class_id: 1,
+              name: 'asdf2',
+              description: 'sdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf',
+              intended_lecture_num: 0,
+              start_time: '2018-05-02T06:03:05.000Z',
+              end_time: null,
+              opened: 1,
+              created_at: '2018-05-15T06:03:06.000Z',
+              updated_at: '2018-05-15T07:12:08.000Z',
+              master_id: 11,
+              user_class: {
+                role: 'teacher',
+                class_id: 1,
+                user_id: 11,
+              },
+            },
+          ]" />
+
     <br />
-    TODO: 과목 개설 버튼을 넣는다<br />
+
+    <div class="right-align">
+      <router-link to="/a/teacher/class/new">
+        <el-button type="primary">과목 개설</el-button>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState, mapMutations, mapActions } from 'vuex';
 import ClassList from '../partials/ClassList';
 
 export default {
@@ -17,5 +56,29 @@ export default {
   components: {
     ClassList,
   },
+  computed: {
+    ...mapState('class', [
+      'currentClassIndex',
+      'teachingClassList',
+      'studyingClassList',
+    ]),
+  },
+  methods: {
+    ...mapMutations('class', [
+      'updateCurrentClassIndex',
+    ]),
+    ...mapActions('class', [
+      'getMyClassLists',
+      'getKnowledgeMapData',
+    ]),
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+#teacher_class_index_wrapper {
+  .right-align {
+    text-align: right;
+  }
+}
+</style>
