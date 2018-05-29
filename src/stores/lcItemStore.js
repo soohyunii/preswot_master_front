ad562ff47efe7b26d6fea2ec666619e548cd98ea
Dong-jin Ahn <adoji92@gmail.com>
2018-05-29 10:11:19 +0900

    Create lcItemStore

---
+++ src/stores/lcItemStore.js
@@ -0,0 +1,26 @@
+// import lectureItemService from '../services/lectureItemService';
+// import fileService from '../services/fileService';
+// import questionService from '../services/questionService';
+// import materialService from '../services/materialService';
+// import surveyService from '../services/surveyService';
+// import homeworkService from '../services/homeworkService';
+// import studentService from '../services/studentService';
+// import practiceService from '../services/practiceService';
+// import discussionService from '../services/discussionService';
+// import { baseUrl } from '../services/config';
+
+// import utils from '../utils';
+
+export default {
+  namespaced: true,
+  state: {
+    isEdit: false,
+    // //////////////////////////절취선////////////////////////// //
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+};
