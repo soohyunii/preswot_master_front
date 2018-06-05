3fd69467055562b55589b24171f2ea9e9b72a39b
Dong-jin Ahn <adoji92@gmail.com>
2018-06-05 16:27:52 +0900

    Implement LcPracticeEditor.vue

--- src/components/partials/LcPracticeEditor.vue
+++ src/components/partials/LcPracticeEditor.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="lc_practice_editor_wrapper">
-    lc practice editor wrapper
+    <el-form-item label="코드">
+      <el-input v-model="inputTail.code" placeholder="내용을 입력해주세요." type="textarea" :rows="10"></el-input>
+    </el-form-item>
   </div>
 </template>
 
 <script>
 export default {
   name: 'LcPracticeEditor',
+  data() {
+    const initialInputBody = {};
+    const initialInputTail = {
+      code: null,
+    };
+    return {
+      initialInputBody,
+      initialInputTail,
+      inputBody: Object.assign({}, initialInputBody),
+      inputTail: Object.assign({}, initialInputTail),
+    };
+  },
 };
 </script>
