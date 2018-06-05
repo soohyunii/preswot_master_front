2bd2629a87da9b779ab28800e4e846ca0eb959dc
Dong-jin Ahn <adoji92@gmail.com>
2018-06-05 12:48:44 +0900

    Add isProd @ services/config

--- src/services/config.js
+++ src/services/config.js
@@ -7,10 +7,14 @@ let backPort = 8000;
 let socketPort = 8001;
 // let backPort = 3000;
 
-
+const isProd = document.URL.includes('preswot');
 const isTest = document.URL.includes('13.124.196.202');
 // const isTest = true;
-if (isTest) {
+if (isProd) {
+  serverUrl = 'http://hoeeeeeeng';
+  backPort = 8000;
+  socketPort = 8001;
+} else if (isTest) {
   serverUrl = 'http://13.124.236.81';
   backPort = 8000;
   socketPort = 8001;
