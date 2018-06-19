1f1325a372d40b58e1945c2848c8342bf30b5611
Dong-jin Ahn <adoji92@gmail.com>
2018-06-19 14:58:31 +0900

    Fix config

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
-// let serverUrl = 'http://localhost';
-let serverUrl = 'http://165.132.105.33';
+let serverUrl = 'http://localhost';
+// let serverUrl = 'http://165.132.105.33';
 
 let backPort = 8000;
 let socketPort = 8001;
