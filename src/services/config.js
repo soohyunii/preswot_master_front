59b5b289ee84653f0d653512b8b33b09f7ac3685
Dong-jin Ahn <adoji92@gmail.com>
2018-06-19 10:57:27 +0900

    Fix services/config.js

--- src/services/config.js
+++ src/services/config.js
@@ -7,9 +7,10 @@ let backPort = 8000;
 let socketPort = 8001;
 // let backPort = 3000;
 
-// const isTest = document.URL.includes('13.124.196.202');
-const isTest = true;
-const isProd = false;
+const isTest = document.URL.includes('13.124.196.202');
+// const isTest = true;
+const isProd = document.URL.includes('preswot');
+// const isProd = true;
 // const isTest = true;
 if (isProd) {
   serverUrl = 'http://13.125.31.75';
