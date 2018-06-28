f897472764d1e20c2306b5c176e752f6f31f6a7e
Dong-jin Ahn <adoji92@gmail.com>
2018-06-28 16:15:53 +0900

    Implement AppFooter

--- src/components/layouts/AppFooter.vue
+++ src/components/layouts/AppFooter.vue
@@ -1,8 +1,47 @@
 <template>
-  <div>
-    <!-- <h1>This is Footer Template.</h1> -->
+  <div id="app_footer_wrapper">
+    <div class="bt-container">
+      <el-row>
+        <el-col :span="24" class="first-section">
+          <h3 class="first-section-title">Preswot</h3>
+          <ul class="first-section-body">
+            <li>
+              <router-link to="/policy">Policy</router-link>
+            </li>
+            <li>
+              <router-link to="/download">Download</router-link>
+            </li>
+          </ul>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import "~@/variables.scss";
+
+#app_footer_wrapper {
+  border-top: lightgray 1px solid;
+  .first-section {
+    text-align: center;
+    h3.first-section-title {
+      margin-bottom: 10px;
+    }
+    ul.first-section-body {
+      margin-top: 6px;
+      list-style-type: none;
+      padding: 0;
+      a {
+        color: $--color-primary;
+        line-height: 1.8em;
+        vertical-align: top;
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
 
 
 <script>
