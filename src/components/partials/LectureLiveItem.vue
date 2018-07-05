fa1ee61c5989837d8a2d1e6705834c394286187a
Dong-jin Ahn <adoji92@gmail.com>
2018-07-05 15:38:58 +0900

    Fix word wrap for LectureLiveItem pre

--- src/components/partials/LectureLiveItem.vue
+++ src/components/partials/LectureLiveItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="background: #e5e9f2">
+  <div class="lecture-live-item-wrapper" style="background: #e5e9f2">
     <div v-if="data === undefined"> <!-- 입력값 없음 -->
       강사님의 신호를 기다리는 중입니다.
     </div>
@@ -143,6 +143,16 @@ export default {
 };
 </script>
 
-<style>
-
+<style lang="scss">
+.lecture-live-item-wrapper {
+  pre {
+    overflow-x: auto; /* Use horizontal scroller if needed; for Firefox 2, not needed in Firefox 3 */
+    white-space: pre-wrap; /* css-3 */
+    white-space: -moz-pre-wrap !important; /* Mozilla, since 1999 */
+    white-space: -pre-wrap; /* Opera 4-6 */
+    white-space: -o-pre-wrap; /* Opera 7 */
+    /* width: 99%; */
+    word-wrap: break-word; /* Internet Explorer 5.5+ */
+  }
+}
 </style>
