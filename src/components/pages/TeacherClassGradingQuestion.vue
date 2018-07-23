8925980803f44e14e7e21bdc65d205da222e2a9a
Min Hyun Lee <lmh315113@gmail.com>
2018-07-23 10:14:20 +0900

    Seperate lecture_submit_list @TeacherGrading(Question/Survey)

--- src/components/pages/TeacherClassGradingQuestion.vue
+++ src/components/pages/TeacherClassGradingQuestion.vue
@@ -3,73 +3,76 @@
     <div v-if="questionResult">
       <h2>{{ currentTeachingClass(classId) ? currentTeachingClass(classId).name : '' }} > {{ questionResult.lectureName }}</h2>
       <br />
+      <br />
       <p><strong>{{ questionResult.itemName }}</strong></p>
       <br />
       <div class="basic-info">
-        <el-row>
-          <el-col :span="4">도구 유형</el-col>
-          <el-col :span="20">문항 > {{ questionResult.type }}</el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="4">문제</el-col>
-          <el-col :span="20">{{ questionResult.question }}</el-col>
-        </el-row>
-        <div v-if="questionResult.type === 'SW'">
+        <el-card>
           <el-row>
-            <el-col :span="4">제출 가능 언어</el-col>
-            <el-col :span="20">{{ questionResult.acceptLanguage.join(', ') }}</el-col>
+            <el-col :span="4">도구 유형</el-col>
+            <el-col :span="20">문항 > {{ questionResult.type }}</el-col>
           </el-row>
           <el-row>
-            <el-col :span="4">입력 설명</el-col>
-            <el-col :span="20">{{ questionResult.inputDescription }}</el-col>
+            <el-col :span="4">문제</el-col>
+            <el-col :span="20">{{ questionResult.question }}</el-col>
+          </el-row>
+          <div v-if="questionResult.type === 'SW'">
+            <el-row>
+              <el-col :span="4">제출 가능 언어</el-col>
+              <el-col :span="20">{{ questionResult.acceptLanguage.join(', ') }}</el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="4">입력 설명</el-col>
+              <el-col :span="20">{{ questionResult.inputDescription }}</el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="4">출력 설명</el-col>
+              <el-col :span="20">{{ questionResult.outputDescription }}</el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="4">샘플 입력</el-col>
+              <el-col :span="20">{{ questionResult.sampleInput }}</el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="4">샘플 출력</el-col>
+              <el-col :span="20">{{ questionResult.sampleOutput }}</el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="4">시간 제한</el-col>
+              <el-col :span="20">{{ questionResult.timeLimit }}초</el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="4">메모리 제한</el-col>
+              <el-col :span="20">{{ questionResult.memoryLimit }}MB</el-col>
+            </el-row>
+          </div>
+          <el-row v-if="questionResult.type === '객관'">
+            <el-col :span="4">보기</el-col>
+            <el-col :span="20">
+              <span class="choices" v-for="(choice, index) in questionResult.choice"><span class="index">{{ index + 1 }} .</span> {{ choice }}</span>
+            </el-col>
+          </el-row>
+          <el-row v-if="['객관', '서술', '단답', 'SQL'].includes(questionResult.type)">
+            <el-col :span="4">정답</el-col>
+            <el-col :span="20">{{ questionResult.answer }}</el-col>
           </el-row>
           <el-row>
-            <el-col :span="4">출력 설명</el-col>
-            <el-col :span="20">{{ questionResult.outputDescription }}</el-col>
+            <el-col :span="4">키워드</el-col>
+            <el-col :span="20">
+              <transition-group type="transition">
+                <el-tag class="tag" type="primary" v-for="k in questionResult.keywords" :key="k.keyword">{{ k.keyword }} / {{ k.score_portion }}</el-tag>
+              </transition-group>
+            </el-col>
           </el-row>
           <el-row>
-            <el-col :span="4">샘플 입력</el-col>
-            <el-col :span="20">{{ questionResult.sampleInput }}</el-col>
+            <el-col :span="4">배점</el-col>
+            <el-col :span="20">{{ questionResult.score }}</el-col>
           </el-row>
-          <el-row>
-            <el-col :span="4">샘플 출력</el-col>
-            <el-col :span="20">{{ questionResult.sampleOutput }}</el-col>
-          </el-row>
-          <el-row>
-            <el-col :span="4">시간 제한</el-col>
-            <el-col :span="20">{{ questionResult.timeLimit }}초</el-col>
-          </el-row>
-          <el-row>
-            <el-col :span="4">메모리 제한</el-col>
-            <el-col :span="20">{{ questionResult.memoryLimit }}MB</el-col>
-          </el-row>
-        </div>
-        <el-row v-if="questionResult.type === '객관'">
-          <el-col :span="4">보기</el-col>
-          <el-col :span="20">
-            <span class="choices" v-for="(choice, index) in questionResult.choice"><span class="index">{{ index + 1 }} .</span> {{ choice }}</span>
-          </el-col>
-        </el-row>
-        <el-row v-if="['객관', '서술', '단답', 'SQL'].includes(questionResult.type)">
-          <el-col :span="4">정답</el-col>
-          <el-col :span="20">{{ questionResult.answer }}</el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="4">키워드</el-col>
-          <el-col :span="20">
-            <transition-group type="transition">
-              <el-tag class="tag" type="primary" v-for="k in questionResult.keywords" :key="k.keyword">{{ k.keyword }} / {{ k.score_portion }}</el-tag>
-            </transition-group>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="4">배점</el-col>
-          <el-col :span="20">{{ questionResult.score }}</el-col>
-        </el-row>
+        </el-card>
       </div>
       <br />
       <br />
-      <n-n-lecture-question-result
+      <lecture-question-result
         :classId="classId"
         :itemId="itemId"
         resultType="결과보기"
@@ -95,10 +98,13 @@
 
 <script>
   import { mapActions, mapState, mapGetters } from 'vuex';
-  import NNLectureQuestionResult from '../partials/NNLectureQuestionResult';
+  import LectureQuestionResult from '../partials/LectureQuestionResult';
 
   export default {
     name: 'TeacherClassGradingQuestion',
+    components: {
+      LectureQuestionResult,
+    },
     data() {
       return {
         activeTab: 'question',
@@ -120,9 +126,6 @@
         const vm = this;
         return Number.parseInt(vm.$route.params.itemId, 10);
       },
-    },
-    components: {
-      NNLectureQuestionResult,
     },
     async created() {
       const vm = this;
---
+++ src/components/partials/LectureQuestionResult.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <div v-if="questionResult">
+      <el-row v-if="resultType === '실시간'">
+        <el-col :span="5"><strong>현재 수강 인원</strong></el-col>
+        <el-col :span="8"> 실시간 수강 인원  / {{ numberOfStudentInClass }} </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="2"><strong>학생 답변</strong></el-col>
+        <el-col :span="8">총 {{ questionResult.numberOfStudent }}건</el-col>
+        <el-button v-if="resultType === '실시간'" style="float:right" type="primary" size="small" icon="el-icon-refresh" @click="refresh()">새로고침</el-button>
+      </el-row>
+      <br />
+      <el-row>
+        <el-col :span="12">
+          <el-table v-if="questionResult.type === '객관'"
+                    :data="questionResult.obAnswers"
+                    :header-cell-style="changeHead"
+                    style="margin-bottom: 20px;">
+            <el-table-column
+              prop="choice"
+              label="보기"
+              align="center">
+            </el-table-column>
+            <el-table-column
+              prop="number"
+              label="선택한 학생 수"
+              align="center">
+            </el-table-column>
+          </el-table>
+        </el-col>
+      </el-row>
+
+      <el-table :data="questionResult.answers"
+                :header-cell-style="changeHead"
+                height="500">
+        <el-table-column
+          label="학생 아이디"
+          align="center"
+          width="250px">
+          <template slot-scope="scope">
+            <p>{{ scope.row.user.email_id }}</p>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="answer"
+          label="답변"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          v-if="questionResult.type === 'SW'"
+          label="결과"
+          align="center">
+          <template slot-scope="scope">
+            <p>{{ scope.row.swResult }}</p>
+            <!-- time 부분에서 null 에러 발생 -->
+            <p>{{ scope.row.oj_solution.time }}ms, {{ scope.row.oj_solution.memory }}KB</p>
+            <p v-if="scope.row.swResult === '컴파일 에러'">{{ scope.row.oj_compileinfo.error }}</p>
+            <p v-if="scope.row.swResult === '런타임 에러'">{{ scope.row.oj_runtimeinfo.error }}</p>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="score"
+          label="점수"
+          width="150px"
+          align="center"
+          sortable>
+            <template slot-scope="scope">
+              <div v-if="questionResult.type !== '서술'">
+                <p v-if="scope.row.score === null">-</p>
+                <p v-else>{{ scope.row.score }}</p>
+              </div>
+              <div v-else>
+                <el-input placeholder="Please input"
+                        v-model="scope.row.score"
+                        type="number"
+                        min="0"
+                        :max="questionResult.score" />
+              </div>
+            </template>
+        </el-table-column>
+        <el-table-column
+          v-if="questionResult.type === '서술'"
+          label=""
+          width="100px"
+          align="center">
+          <template slot-scope="scope">
+            <el-button type="info"
+                      plain
+                      size="small"
+                      @click="scoreSubmit(scope.row.score, scope.row.student_answer_log_id)">
+              확인
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .choices {
+    padding-right: 25px;
+    .index {
+      color: #888888;
+    }
+  }
+  .el-row {
+    padding-bottom: 0px;
+    padding-left: 0px;
+    padding-right: 0px;
+  }
+</style>
+
+
+<script>
+  import { mapActions, mapState, mapGetters } from 'vuex';
+
+  export default {
+    name: 'LectureQuestionResult',
+    props: ['classId', 'itemId', 'resultType'],
+    data() {
+      return {
+        activeTab: 'question',
+      };
+    },
+    computed: {
+      ...mapState('grading', [
+        'theResult',
+        'questionResult',
+      ]),
+      ...mapGetters('NNclass', [
+        'currentTeachingClass',
+      ]),
+      ...mapGetters('class', [
+        'numberOfStudentInClass',
+      ]),
+    },
+    async created() {
+      const vm = this;
+    //   if (!vm.theResult) {
+      // await vm.getClassTotalResult({
+      //   classId: vm.classId,
+      // });
+    //   }
+      vm.getQuestionResult({
+        itemId: vm.itemId,
+      });
+    },
+    methods: {
+      ...mapActions('grading', [
+        // 'getClassTotalResult',
+        'getQuestionResult',
+      ]),
+      ...mapActions('NNclass', [
+        'putScore',
+      ]),
+      async refresh() {
+        const vm = this;
+        // await vm.getClassTotalResult({
+        //   classId: vm.classId,
+        //   isStudent: false,
+        // });
+        vm.getQuestionResult({
+          itemId: vm.itemId,
+        });
+      },
+      changeHead() {
+        return { backgroundColor: '#EAEAEA' };
+      },
+      async scoreSubmit(score, id) {
+        const vm = this;
+        if (score < 0 || score > vm.questionResult.score) {
+          vm.$notify({
+            title: '점수 범위를 확인하세요',
+            message: '',
+            type: 'error',
+            duration: 2000,
+          });
+          return;
+        }
+        const res = await vm.putScore({ id, score });
+        if (res && res.status === 200) {
+          vm.$notify({
+            title: '완료',
+            type: 'success',
+            duration: 1000,
+          });
+        }
+      },
+    },
+  };
+</script>
