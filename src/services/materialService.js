d7a128c4b33e474dd974281f92eadd62ce152f19
Unknown <wastenottears@gmail.com>
2018-07-24 14:33:41 +0900

    reset
    material 관련으로 작업하던거 다시 돌려놓음

--- src/services/materialService.js
+++ src/services/materialService.js
@@ -1,5 +1,4 @@
 import http from './http';
-import utils from '../utils';
 
 export default {
   postMaterial({
@@ -9,38 +8,24 @@ export default {
       lectureItemId,
     });
   },
-  // postMaterialFile({
-  //   materialId,
-  //   file,
-  // }) {
-  //   const form = new FormData();
-  //   form.append('file', file, file.name);
-  //   return http.post(`/materials/${materialId}/file`, form, {
-  //     headers: {
-  //       'Content-Type': 'multipart/form-data',
-  //     },
-  //   });
-  // },
+  postMaterialFile({
+    materialId,
+    file,
+  }) {
+    const form = new FormData();
+    form.append('file', file, file.name);
+    return http.post(`/materials/${materialId}/file`, form, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  },
   putMaterial({
     materialId,
-    link,
-    clip_start,
-    clip_end,
+    score,
   }) {
-    const param = {};
-
-    utils.assignIfNotNil(param, { link });
-    utils.assignIfNotNil(param, { clip_start });
-    utils.assignIfNotNil(param, { clip_end });
-
-    return http.put(`/materials/${materialId}`, param);
-  },
-  putMaterialType({
-    materialId,
-    type,
-  }) {
-    return http.put(`/materials/${materialId}/type`, {
-      type,
+    return http.put(`/materials/${materialId}`, {
+      score,
     });
   },
 };
