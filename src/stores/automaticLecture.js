5817250285329f328a075d2320312da905cf272a
Min Hyun Lee <lmh315113@gmail.com>
2018-08-21 15:26:32 +0900

    Add automaticLecture store/service

---
+++ src/stores/automaticLecture.js
@@ -0,0 +1,13 @@
+import automaticLectureService from '../services/automaitcLectureService';
+
+export default {
+  namespaced: true,
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+};
