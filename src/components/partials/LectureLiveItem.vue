d955b1d18ff7db4ed765375faafb4a214f398e6b
JungHo Lee <subwaycoder@gmail.com>
2018-09-12 11:42:28 +0900

    실시간 문항 잘림 hotfix

--- src/components/partials/LectureLiveItem.vue
+++ src/components/partials/LectureLiveItem.vue
@@ -489,14 +489,16 @@ export default {
 .lecture-live-item-wrapper {
   pre {
     overflow-x: auto; /* Use horizontal scroller if needed; for Firefox 2, not needed in Firefox 3 */
-    white-space: pre-wrap; /* css-3 */
+    white-space: pre-line; /* css-3 */
     white-space: -moz-pre-wrap !important; /* Mozilla, since 1999 */
     white-space: -pre-wrap; /* Opera 4-6 */
     white-space: -o-pre-wrap; /* Opera 7 */
     /* width: 99%; */
     word-wrap: break-word; /* Internet Explorer 5.5+ */
   }
-
+  .el-checkbox__label {
+    white-space: pre-line; /* css-3 */
+  }
   .radio-one {
     /*border:1px solid red;*/
     margin:15px;
