4cc4368d2e9e7798a2b7abd138c8413759d5ed0c
JungHo Lee <subwaycoder@gmail.com>
2018-11-06 17:45:24 +0900

    코드정리, 실시간 - lectureID null 버그 2차 수정

--- src/components/pages/NNTeacherLectureLive.vue
+++ src/components/pages/NNTeacherLectureLive.vue
@@ -214,12 +214,14 @@ export default {
   },
   mounted() {
     const vm = this;
-    window.addEventListener('beforeunload', vm.beforeunloadFunc);
+    vm.localLectureId = vm.lectureId; // 선언부 주석 참조
+    window.addEventListener('beforeunload', vm.beforeLeave);
   },
   data() {
     return {
       tableItemList: [],
       tableItemIndex: [],
+      localLectureId: '', // beforeDestory에서 vm.$route.params.lectureId 를 못읽는 문제가 발생하여 임시 저장
       currentLectureItemId: [],
       path: '',
       isAuto: false,
@@ -363,19 +365,20 @@ export default {
         }
       }
     },
-    beforeunloadFunc() {
+    beforeLeave() {
       const vm = this;
       const param = {
-        lecture_id: vm.lectureId,
+        lecture_id: vm.localLectureId,
         user_id: utils.getUserIdFromJwt(),
       };
       vm.$socket.emit('LEAVE_LECTURE', JSON.stringify(param));
+
+      // 화면 떠나기 전 등록한 Listener 해제. 이 코드가 없으면 리스너가 중복 등록되어 버그가 발생함
+      window.removeEventListener('beforeunload', vm.beforeLeave);
     },
   },
   beforeDestroy() {
     const vm = this;
-    // 화면 떠나기 전 등록한 Listener 해제. 이 코드가 없으면 리스너가 여러개 등록되어 null값이 전송됨
-    window.removeEventListener('beforeunload', vm.beforeunloadFunc);
     // 강사가 강의 화면에서 나가는 경우, 열려있는 아이템을 모두 닫는 동작
     const params = [];
     vm.currentLectureItemId.forEach((item) => {
@@ -387,11 +390,7 @@ export default {
       params.push(param);
     });
     vm.$socket.emit('LECTURE_ITEMS_ACTIVATION', JSON.stringify(params));
-    const param2 = {
-      lecture_id: vm.lectureId,
-      user_id: utils.getUserIdFromJwt(),
-    };
-    vm.$socket.emit('LEAVE_LECTURE', JSON.stringify(param2));
+    vm.beforeLeave();
     vm.$socket.close();
     clearInterval(vm.sOnStudentCount);
   },
