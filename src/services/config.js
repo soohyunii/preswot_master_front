555866b9585d4fd43cd6387f7f624e21d31ad3dc
unknown <kimsmhl94@gmail.com>
2018-12-21 17:29:49 +0900

    강사 강의 종료 수정

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 
-let serverUrl = 'http://13.125.182.116';
+const serverUrl = 'http://13.125.182.116';
 
-let backPort = 8090;
-let socketPort = 8091;
+const backPort = 8090;
+const socketPort = 8091;
 /*
 const isTest = document.URL.includes('13.124.196.202');
 const isProd = document.URL.includes('preswot');
